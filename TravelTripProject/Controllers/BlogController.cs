704e292dd3768d28ff4c7e94aeef14842cd2f08f
YakupAlmatepe <almatepe.yakup@gmail.com>
2023-05-17 18:09:49 +0300

    Add project files.

---
+++ TravelTripProject/Controllers/BlogController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProject.Controllers;
+using TravelTripProject.Models.Sınıflar;
+
+namespace TravelTripProject.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var bloglar = c.Blogs.ToList();
+            return View(bloglar);
+        }
+    }
+}
